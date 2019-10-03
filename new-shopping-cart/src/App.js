4c11369b4b0d0bc5e461643e42c93be5f75dd421
haolinli96 <lihaolin@u.northwestern.edu>
2019-10-03 17:44:52 -0500

    interaction1

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import 'rbx/index.css'; //import in react file need styling
-import { Button, Container, Title, Message, Column, Card, Image, Content, Block } from 'rbx'; //and specify the components
+import { Button, Container, Title, Message, Column, Card, Image, Content, Block, Modal, Box } from 'rbx'; //and specify the components
 
 import firebase from 'firebase/app';
 import 'firebase/database';
@@ -18,11 +18,13 @@ const Banner = () => (
   </React.Fragment>
 );
 
-
+//use badge for free shipping
 const Product = ({ product }) => (
   <React.Fragment>
     <Column size="one-quarter"> 
-      <Card textAlign={"centered"}>
+      <Card textAlign={"centered"} 
+        tooltip={ product.isFreeShipping ? "Free shipping !" : "Shipping: $3" }
+      >
         <Card.Header>
           <Card.Header.Title>{ product.title }</Card.Header.Title>
         </Card.Header>
@@ -57,11 +59,17 @@ const Product = ({ product }) => (
   </React.Fragment>
 );
 
+const buttonColor = selected => (
+  selected ? 'success' : null
+);
 
-const SizeSelector = () => (
+const SizeSelector = ({ state }) => (
   <Button.Group>
     { sizes.map(size =>
-        <Button rounded key={size}
+        <Button color= { buttonColor(state.sizeList.includes(size)) } 
+        rounded key={size}
+        onClick={ ()=> state.toggle(size) }
+        
         >
           { size }
         </Button>
@@ -70,11 +78,21 @@ const SizeSelector = () => (
   </Button.Group>
 );
 
+const useSizeSelection = () => {
+  const [sizeList, setSizeList] = useState(sizes);
+  const toggle = (x) => {
+    setSizeList(sizeList.includes(x) ? sizeList.filter(y => y !== x) : [x].concat(sizeList))
+  };
+  return [ sizeList, toggle ];
+};
 
 const ProductList = ({ products }) => {
+  const [sizeList, toggle] = useSizeSelection(sizes);
+  
+
   return (
     <React.Fragment>
-      <SizeSelector />
+      <SizeSelector state={ { sizeList, toggle } }/>
       <Column.Group multiline >
         { products.map(product =>
           <Product key={ product.sku } product={ product } 
@@ -84,9 +102,70 @@ const ProductList = ({ products }) => {
   );
 };
 
+
+const SelectedItem = ({ selectedItem }) => (
+  <Box>
+
+  </Box>
+);
+
+//overlay
+//use a card modal for this
+const ShoppingCart = ({ selected, visible }) => {
+ return (
+  
+    <Modal modalProps={{ closeOnBlur: true, document }}>
+      <Modal.Background />
+      <Modal.Content>
+        <Image.Container as="p" size="4by3">
+          <Image src="https://bulma.io/images/placeholders/1280x960.png" />
+        </Image.Container>
+      </Modal.Content>
+      <Modal.Close />
+    </Modal>
+  /* <Modal>
+     { <Modal.Background />
+     <Modal.Content>
+       { selected.map(selectedItem =>
+        <SelectedItem key={ selectedItem.sku } selectedItem={ selectedItem }/>)}
+     </Modal.Content> }
+    </Modal> */
+ )
+}; 
+
+const useSelection = () => {
+  const [selected, setSelected] = useState({}); //selected是一个list
+  const addProduct = (x) => { //x is product
+    //setSelected(selected)
+    //create new with quantity=1 when not included
+    //delete old and create new with quantity += 1 when included
+    //both do insersion
+    var tempQuantity = { quantity: 1 };
+    var tempSelectedItem = selected.find(selectedItem => 
+                            selectedItem.sku === x.sku);
+    //tempSelectedItem {sku: 123, title: ..., q: 2}
+    setSelected(selected.filter(y => y !== tempSelectedItem));
+    //without tempSelectedItem
+    if (Object.keys(selected).includes(x.sku))  tempSelectedItem.quantity += 1;
+    else  tempSelectedItem = Object.assign(tempQuantity, x);
+    setSelected([tempSelectedItem].concat(selected));
+  };
+  const deleteProduct = (x) => {
+    setSelected(selected.filter(y => y.sku !== x.sku));
+
+  };
+  return [selected, addProduct, deleteProduct];
+}; 
+
+
 const App = () => {
   const [data, setData] = useState({});
   const products = Object.values(data);
+
+  const [visible, setVisible] = useState(false);
+  const [selected, addProduct, deleteProduct] = useSelection();
+  //the x to be given should be sku
+
   useEffect(() => {
     const fetchProducts = async () => {
       const response = await fetch('./data/products.json');
@@ -97,11 +176,47 @@ const App = () => {
   }, []);
 
   return (
+    
+      //<ShoppingCart selected={ selected } visible={ visible }/>
     <Container>
       <Banner />
       <ProductList products={ products }/>
-    </Container>
+    </Container> 
   );
 };
 // {products.map(product => <li key={product.sku}>{product.title}</li>)}
 export default App;
+
+
+
+
+
+
+
+
+/* selected
+  [
+   {
+      "sku": 51498472915966370,
+      "title": "Dark Thug Blue-Navy T-Shirt",
+      "description": "",
+      "style": "Front print and paisley print",
+      "price": 29.45,
+      "currencyId": "USD",
+      "currencyFormat": "$",
+      "isFreeShipping": true
+      "quantity": 1
+    },
+   {
+      sku: 10686354557628304,
+      title: "Sphynx Tie Dye Wine T-Shirt",
+      description: "GPX Poly 1",
+      style: "Front tie dye print",
+      price: 9,
+      currencyId: "USD",
+      currencyFormat: "$",
+      isFreeShipping: true
+      quantity: 3
+    }
+]
+*/
