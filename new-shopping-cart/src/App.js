d6dee6967da276c31582270296ac3d2674c822df
haolinli96 <lihaolin@u.northwestern.edu>
2019-10-02 22:22:58 -0500

    cards of products

--- new-shopping-cart/src/App.js
+++ new-shopping-cart/src/App.js
@@ -1,26 +1,72 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
+import 'rbx/index.css'; //import in react file need styling
+import { Button, Container, Title, Message, Column, Card, Image, Content } from 'rbx'; //and specify the components
 
-function App() {
+import firebase from 'firebase/app';
+import 'firebase/database';
+import "firebase/auth";
+import "firebase/firestore";
+import 'firebase/auth';
+import { ColumnGroup } from 'rbx/grid/columns/column-group';
+
+//
+const Banner = () => (
+  <React.Fragment>
+    <Title>{ 'Shopping Cart' }</Title>
+  </React.Fragment>
+);
+
+
+
+const Product = ({ product }) => (
+  <React.Fragment>
+    <Column size="one-quarter"> 
+      <Card>
+        <Card.Image>
+          <Image.Container size={64} >
+            <Image src={`./data/products/${product.sku}_2.jpg`}/>
+          </Image.Container>
+        </Card.Image>
+        <Card.Content>
+          <Content>
+            { product.title }
+          </Content>
+        </Card.Content>
+      </Card>
+    </Column>
+  </React.Fragment>
+);
+
+const ProductList = ({ products }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Column.Group multiline >
+        { products.map(product =>
+          <Product key={ product.sku } product={ product } 
+          />) }
+      </Column.Group>
+    </React.Fragment>
   );
-}
+};
 
+const App = () => {
+  const [data, setData] = useState({});
+  const products = Object.values(data);
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const response = await fetch('./data/products.json');
+      const json = await response.json();
+      setData(json);
+    };
+    fetchProducts();
+  }, []);
+
+  return (
+    <Container>
+      <Banner />
+      <ProductList products={ products }/>
+    </Container>
+  );
+};
+// {products.map(product => <li key={product.sku}>{product.title}</li>)}
 export default App;
